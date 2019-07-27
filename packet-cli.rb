4a84c7e0fe6a246b26458aa40bf4ea41ad61c617
Joel Hermanns <joel.hermanns@gmail.com>
2019-07-27 13:29:52 +0200

    fix(packet-cli): fix installation name

--- packet-cli.rb
+++ packet-cli.rb
@@ -5,6 +5,9 @@ class PacketCli < Formula
   sha256 "a98e96ea3e83b0470ff74c992c26560a6948041c07d0f18410c962a26e71b802"
 
   def install
+    # install binary
     bin.install "packet-darwin-amd64"
+    # rename binary to make it accessible as packet
+    mv bin/"packet-darwin-amd64", bin/"packet"
   end
 end
