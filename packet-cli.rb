f2b7205e25dfff5bef760c4f9416c2abdcf4bad7
Joel Hermanns <joel.hermanns@gmail.com>
2019-09-08 16:22:51 +0200

    feat(packet-cli): upgrade to version 0.0.3

--- packet-cli.rb
+++ packet-cli.rb
@@ -1,8 +1,9 @@
 class PacketCli < Formula
   desc "Official Packet CLI"
-  homepage ""
-  url "https://github.com/packethost/packet-cli/releases/download/0.0.2/packet-darwin-amd64"
-  sha256 "a98e96ea3e83b0470ff74c992c26560a6948041c07d0f18410c962a26e71b802"
+  homepage "https://github.com/packethost/packet-cli"
+  url "https://github.com/packethost/packet-cli/releases/download/0.0.3/packet-darwin-amd64"
+  sha256 "83c6039b5ba49476f4fc2de4f53c042509ca6f4230e009382a42247d77f5e95f"
+  version "0.0.3"
 
   def install
     # install binary
