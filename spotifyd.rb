4ff5b9fb0d984b88ae834116b0d8aa76f88596d9
Joel Hermanns <joel.hermanns@gmail.com>
2019-11-05 06:56:23 +0100

    feat(spotifyd): update to v0.2.20

--- spotifyd.rb
+++ spotifyd.rb
@@ -1,9 +1,9 @@
 class Spotifyd < Formula
   desc "spotifyd"
   homepage "https://github.com/Spotifyd/spotifyd"
-  url "https://github.com/Spotifyd/spotifyd/releases/download/v0.2.19/spotifyd-macos-slim.tar.gz"
-  sha256 "074dc1a316df8b67685d092e8a5567d22c47d10bc3bea7273997b587e78a7eab"
-  version "0.2.19"
+  url "https://github.com/Spotifyd/spotifyd/releases/download/v0.2.20/spotifyd-macos-slim.tar.gz"
+  sha256 "200b3ea60b32149e0b0d706982f0133377a4625eeefacf0d1f96494f33764dcba6195b0f2c108428f27cad8abbab4374e9f5f29dfd14cb85c222e7aa671b881f"
+  version "0.2.20"
 
   def install
     # install binary
