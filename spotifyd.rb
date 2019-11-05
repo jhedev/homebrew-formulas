4833d841feb01891a242705d5be936de2e1a544c
Joel Hermanns <joel.hermanns@gmail.com>
2019-11-05 07:40:16 +0100

    fix(spotifyd): fix checksum

--- spotifyd.rb
+++ spotifyd.rb
@@ -2,7 +2,7 @@ class Spotifyd < Formula
   desc "spotifyd"
   homepage "https://github.com/Spotifyd/spotifyd"
   url "https://github.com/Spotifyd/spotifyd/releases/download/v0.2.20/spotifyd-macos-slim.tar.gz"
-  sha256 "200b3ea60b32149e0b0d706982f0133377a4625eeefacf0d1f96494f33764dcba6195b0f2c108428f27cad8abbab4374e9f5f29dfd14cb85c222e7aa671b881f"
+  sha256 "64136646ca2ed083fe201dd715660587f4596ff386ed07881024e1f16280225a"
   version "0.2.20"
 
   def install
