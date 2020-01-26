f32ddca0d63cab80c7d2d5e60ddf3ea729aef414
Joel Hermanns <joel.hermanns@gmail.com>
2020-01-26 14:31:46 +0100

    fix(spotifyd): fix sha256 sum of archive

--- spotifyd.rb
+++ spotifyd.rb
@@ -2,7 +2,7 @@ class Spotifyd < Formula
   desc "spotifyd"
   homepage "https://github.com/Spotifyd/spotifyd"
   url "https://github.com/Spotifyd/spotifyd/releases/download/v0.2.24/spotifyd-macos-slim.tar.gz"
-  sha256 "5924836b826eb0bc4f89bfca4096026f20ba7af4d9b6233bd21a790c821cc281d8ed8c877548052727ed88b0ffafdbbfc789b421fd6941de849d7c9e07c11fde"
+  sha256 "307ea78fde78ab997f2766f0ef3c70d820e09a88b05271082e7c62f3cb26e2cc"
   version "0.2.24"
 
   def install
